98326b7574e7a4b5443457b1bf11dc6a7dfd5e51
linah5 <laina8j@gmail.com>
2024-10-19 12:15:12 +0300

    Initial Commit

---
+++ Water drinking Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water drinking Tracker
+//
+//  Created by Linah on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water drinking Tracker/Water_drinking_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_drinking_TrackerApp.swift
+//  Water drinking Tracker
+//
+//  Created by Linah on 16/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Water_drinking_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
